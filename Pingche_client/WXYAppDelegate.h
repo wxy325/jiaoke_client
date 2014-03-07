b8602b6be1796ac068689b313cf46cd21638019e
wxy325 <wxy325@qq.com>
2014-03-08 00:03:07 +0800

    Initial Commit

---
+++ Pingche_client/WXYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WXYAppDelegate.h
+//  Pingche_client
+//
+//  Created by wxy325 on 3/8/14.
+//  Copyright (c) 2014 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pingche_client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pingche_client
+//
+//  Created by wxy325 on 3/8/14.
+//  Copyright (c) 2014 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WXYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WXYAppDelegate class]));
+    }
+}
